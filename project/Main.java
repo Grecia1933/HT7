66147c1194f9b712a9fb5d3aae778db59cc7e2e1
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-03-24 21:28:07 -0600

    creacion

---
+++ project/Main.java
@@ -0,0 +1,14 @@
+package project;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("TRADUCTOR DE INGLES-ESPANOL-FRANCES");
+
+
+    }
+    
+}
